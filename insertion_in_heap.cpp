5e4e5c01e076d5d408f284028dd42ba30715a990
abu jafar <abujafarshiddik123@gmail.com>
2023-02-20 16:09:40 +0600

    heap deletion implement

--- insertion_in_heap.cpp
+++ insertion_in_heap.cpp
@@ -11,6 +11,7 @@ public:
     MaxHeap()
     {
     }
+    // up heapify O(logn) beaceuse complete binary trees level h(logn)
     void up_heapify(int idx)
     {
         while (idx > 0 && nodes[idx] > nodes[(idx - 1) / 2])
@@ -19,10 +20,37 @@ public:
             idx = (idx - 1) / 2;
         }
     }
+    // insert O(n)
     void inseart(int x)
     {
         nodes.push_back(x);
         up_heapify(nodes.size() - 1);
+    }
+    // downify
+    void down_hepify(int idx)
+    {
+        while (1)
+        {
+            int largest = idx;
+            int l = idx * 2 + 1;
+            int r = idx * 2 + 2;
+            if (l < nodes.size() && nodes[largest] < nodes[l])
+                l = largest;
+            if (r < nodes.size() && nodes[largest] < nodes[r])
+                r = largest;
+            if (largest == idx)
+                break;
+            swap(nodes[idx], nodes[largest]);
+            idx = largest;
+        }
+    }
+    // delete index
+    void Delete(int idx)
+    {
+        if (idx >= nodes.size())
+            return;
+        swap(nodes[idx], nodes[nodes.size() - 1]);
+        nodes.pop_back();
     }
 };
 int main()
@@ -36,6 +64,10 @@ int main()
 
     for (auto it : h.nodes)
         cout << it << " ";
-    cout<<"\n";
+    cout << "\n";
+    h.Delete(2);
+    for (auto it : h.nodes)
+        cout << it << " ";
+    cout << "\n";
     return 0;
 }
